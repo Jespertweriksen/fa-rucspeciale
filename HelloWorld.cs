da519d149d4a97d3adc892e5961753c6ffca9899
Jesper Eriksen <jespereriksen@Jespers-MacBook-Pro.local>
2021-08-26 16:34:12 +0200

    added service bus configuration

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,6 +7,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.Storage;
 
 namespace RUCSpecialeFunctionProject
 {
@@ -15,15 +16,19 @@ namespace RUCSpecialeFunctionProject
         [FunctionName("Test")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [ServiceBus(queueOrTopicName: "rucspeciale-queue", Connection = "SBConnection")] IAsyncCollector<dynamic> addToQueue,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["test"];
+            string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            await addToQueue.AddAsync($"Name passed is: {name}");
+            log.LogDebug($"Sent message to the queue 'rucspeciale-queue' with the message: {name}");
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
