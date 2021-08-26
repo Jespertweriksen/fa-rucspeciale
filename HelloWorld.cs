752f9cac85083765787e44b817a038c00e84558f
Jesper Eriksen <jespereriksen@Jespers-MacBook-Pro.local>
2021-08-26 15:23:10 +0200

    test

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -19,7 +19,7 @@ namespace RUCSpecialeFunctionProject
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name = req.Query["test"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
