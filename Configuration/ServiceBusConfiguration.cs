62ffea42e31d98edc01eb4183ce1c5905cb7bd2f
Jesper Eriksen <jespereriksen@Jespers-MacBook-Pro.local>
2021-08-26 16:46:10 +0200

    config

---
+++ Configuration/ServiceBusConfiguration.cs
@@ -0,0 +1,33 @@
+namespace DN.AzureFunctions.Configuration
+{
+    /// <summary>
+    /// ServiceBus Topic Definitions. 
+    /// Generated from T4 template ServiceBusConfigurations.tt  
+    /// </summary>
+    public static class ServiceBusTopics {
+     /// <summary>
+    /// Generated from T4 template ServiceBusConfigurations.tt  
+    /// </summary>
+    }
+    /// <summary>
+    /// ServiceBus Queue Definitions.
+    /// Generated from T4 template ServiceBusConfigurations.tt  
+    /// </summary>
+    public static class ServiceBusQueues {
+    /// <summary>
+    /// Generated from T4 template ServiceBusConfigurations.tt  
+    /// </summary>
+      
+    }
+    /// <summary>
+    /// ServiceBus Subscription Definitions.
+    /// Generated from T4 template ServiceBusConfigurations.tt  
+    /// </summary>
+    public static class ServiceBusTopicsSubscriptions {
+    /// <summary>
+    /// Generated from T4 template ServiceBusConfigurations.tt  
+    /// </summary>
+      
+    }
+}
+
